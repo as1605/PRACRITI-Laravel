5249d56fdb1cc807220d4fbca97512e072463978
as1605 <1605.aditya.singh@gmail.com>
2021-05-10 12:39:18 +0530

    About page, footer

--- pracriti-website/resources/views/about.blade.php
+++ pracriti-website/resources/views/about.blade.php
@@ -14,11 +14,103 @@ About
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">
-                    About Us
+        <div class="card text-center my-2">
+            <div class="card-header">
+                <h2>PRACRITI 2.0</h2>
+            </div>
+            <div class="card-body">
+                <p class="card-text">PRACRITI, the acronym for Prediction, Resources, and Assessment of CoRona Infections and Transmission in India, is a mobile-friendly web portal that is freely accessible to everyone. It gives detailed state-wise predictions of COVID-19 and aims to provide details of verified resources for COVID-19 patients, including Oxygen cylinders, Hospital beds, Plasma donors, Online consultation, Vaccine availability and Helpline numbers across India. PRACRITI is developed and maintained by a group of volunteers from IIT Delhi. </p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <h4 class="m-0">Faculty Mentors</h4>
+                    Prof. N. M. Anoop Krishnan, Indian Institute of Technology Delhi
+                    <br>Prof. Amit Sharma, International Centre for Genetic Engineering and Biotechnology 
+                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">Technical Support</h4>
+                    Dr. Manish Agarwal, Computer Services Center, Indian Institute of Technology Delhi
+                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">Contributers</h4>
+                    Ravinder, PhD Research Scholar, Multiphysics & Multiscale Mechanics Research Group, IIT Delhi
+                    <br>Aditya Singh, BTech First Year Student, IIT Delhi
+                    <br>Anurag Gupta, BTech First Year Student, IIT Delhi
+                    <br>Asmita Mehta, BTech First Year Student, IIT Delhi
+                    <br>Aviral Gupta, BTech First Year Student, IIT Delhi
+                    <br>Deepit Patil, BTech First Year Student, IIT Delhi
+                    <br>Sanat Vohra, BTech First Year Student, IIT Delhi
+                    <br>Shashwat Gokhe, BTech First Year Student, IIT Delhi
+                    <br>Radhika Agwan, BTech First Year Student, IIT Delhi
+                </li>
+            </ul>
+        </div>
+        <div class="card text-center my-2">
+            <div class="card-header">
+                <h2>FAQs</h2>
+            </div>
+            <div class="card-body p-0">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/68IOMBMeaoY" allowfullscreen></iframe>
                 </div>
             </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <h4 class="m-0">How are the predictions made in PRACRITI?</h4>
+                    The COVID-19 predictions are based on a recent mathematical model, namely, Adaptive, Interacting, Cluster-based, Susceptible, Exposed, Infected, Removed (AICSEIR) model. This is a modified form of the traditional SEIR model and it caters to the interactions between sub-populations such as districts or states. It represents a more realistic approach towards the prediction of the COVID-19 trajectory than the traditional SEIR models. 
+                    <br>Details of the model can be found here: <a target="_blank" href="https://www.sciencedirect.com/science/article/pii/S2405844020325652">https://www.sciencedirect.com/science/article/pii/S2405844020325652</a>
+                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">What details are used for developing the PRACRITI predictions?</h4>
+                    PRACRITI requires the population of each state, the distance between the capital cities of the states, the number of tourists visiting a state, and the initial infected population in a district (this could be zero as well).
+                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">When was PRACRITI launched?</h4>
+                    PRACRITI was launched in April 2020 and has been continuously maintained eversince. 
+                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0"> How frequently is the data and predictions updated?</h4>
+                    The predictions in PRACRITI are updated every week. It was last updated on May 06, 2021                
+                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">What is the meaning of R0?</h4>
+                    In epidemiology, R0 is known as the basic reproduction number. R0 of infection refers to the number of people who are newly infected from a currently infected person. Reducing the R0 is the key to reducing the transmission of infections.                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">What is the uniqueness of the PRACRITI?</h4>
+                    PRACRITI is the only model, to the best of our knowledge, which provides the state-wise R0 values on a continuous basis. This enables the identification of the rate of spread of the COVID19 in each of the states in India.                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">How is PRACRITI useful?</h4>
+                    The predictions provided by PRACRITI can potentially guide governmental authorities, healthcare professionals, NGOs, and the general public on devising effective strategies for control and mitigation of COVID19. Using the Resources section, people can directly find resources like oxygen cylinders, hospital beds, plasma donors, vaccine centers and their availability and much more
+                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">How accurate are these predictions?</h4>
+                    The accuracy of the predictions will depend on the variations in the parameters, including different lockdown strategies implemented, weather variations, testing kits availability, and the development of any remedial measures. To account for these variabilities, the predictions are updated every week. 
+                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">Can PRACRITI be applied to other countries also?</h4>
+                    Yes, this model is flexible and can be applied to other regions/countries with suitable straightforward adaptations.                </li>
+                <li class="list-group-item">
+                    <h4 class="m-0">What is the relevance of PRACRITI to other countries?</h4>
+                    PRACRITI is a tool that will enable governments, healthcare authorities, and other agencies to develop efficient resource allocation strategies and control the spread of infection.
+                </li>
+            </ul>
+        </div>
+        <div class="card text-center my-2">
+            <div class="card-header">
+                <h2>References</h2>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                Ravinder, R. et al. An Adaptive, Interacting, Cluster-based Model Accurately Predicts the Transmission Dynamics of COVID-19, 2020. <a target="_blank" href="https://www.medrxiv.org/content/10.1101/2020.04.21.20074211v1">(link)</a>
+                </li>
+                <li class="list-group-item">
+                Keeling, M. J. & Rohani, P. Modeling Infectious Diseases in Humans and Animals. (Princeton University Press, 2011).
+                </li>
+                <li class="list-group-item">
+                Adam, D. Special report: The simulations driving the world’s response to COVID-19. Nature 580, 316–318 (2020).
+                </li>
+            </ul>
+        </div>
         </div>
     </div>
 </div>
--- pracriti-website/resources/views/layouts/app.blade.php
+++ pracriti-website/resources/views/layouts/app.blade.php
@@ -32,65 +32,39 @@
             <div class="m-2" style="font-family: 'Samarkan Normal', 'Samarkan', sans-serif;"><h1>PRACRITI 2.0</h1></div>
         </div>
     </header>
+
     <nav class="navbar navbar-expand">
         @yield('nav')
     </nav> 
-    <!--
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar 
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar 
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links 
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+    <main>
+        @yield('content')
+    </main>
+    
+    <footer class="page-footer font-small text-white pt-4" style="background-color: #c21717;">
+        <div class="container-fluid text-center text-md-left">
+            <div class="row">
+                <div class="col-md-6 mt-md-0 mt-3">
+                    <h5 class="text-uppercase">PRACRITI 2.0 | IIT Delhi</h5>
+                    <p>If you have any queries, comments or suggestions, please write to pracriti@iitd.ac.in with subject line "PRACRITI: Query". We, at PRACRITI, are working hard to provide the best projections to join the fight against COVID19. We request you to participate in making this initiative better––if you notice any discrepancy either in the data or projections, please do let us know so that we can correct it appropriately.</p>
+                </div>
+                <hr class="clearfix w-100 d-md-none pb-3">
+                <div class="col-md-3 mb-md-0 mb-3">
+                </div>
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <h5 class="text-uppercase">Links</h5>
+                    <ul class="list-unstyled">
+                        <li><a class="text-white" href="{{url('/resources')}}">Resources</a></li>
+                        <li><a class="text-white" href="{{url('/prediction')}}">Prediction</a></li>
+                        <li><a class="text-white" href="{{url('/resources/helplines')}}">Helplines</a></li>
+                        <li><a class="text-white" href="{{url('/about')}}">About</a></li>
                     </ul>
                 </div>
             </div>
-        </nav>
-        -->
-        <main>
-            @yield('content')
-        </main>
-    </div>
+        </div>
+        <div class="footer-copyright text-center py-3">
+            © 2021 Copyright: <a href="http://iitd.ac.in/">IIT Delhi</a>
+        </div>
+    </footer>
 </body>
 </html>
